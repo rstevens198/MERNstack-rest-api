e89715f721a02d1b9aef4318954150caa84fb798
rstevens198 <ryanstevens198@yahoo.com>
2022-09-01 21:09:54 -0700

    added in rhe auth and the register page.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,20 +3,25 @@ import Dashboard from "./pages/Dashboard";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Header from "./components/Header";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <>
-      <Router>
-        <div class name="container">
-          <Header />
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-          </Routes>
-        </div>
-      </Router>
+      <div className="App">
+        <Router>
+          <div class name="container">
+            <Header />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+            </Routes>
+          </div>
+        </Router>
+      </div>
+      <ToastContainer position="top-center" />
     </>
   );
 }
---
+++ frontend/src/components/Spinner.jsx
@@ -0,0 +1,9 @@
+function Spinner() {
+  return (
+    <div className='loasingSpinnerContainer'>
+        <div className='loadingSpinner'></div>
+    </div>
+  )
+}
+
+export default Spinner
