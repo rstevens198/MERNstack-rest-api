8dcd45cc0658a9a2be0a370dbd54f77ea0bb5867
rstevens198 <ryanstevens198@yahoo.com>
2022-09-01 22:13:24 -0700

    added in the login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,11 +1,34 @@
 import{useState, useEffect} from 'react'
 import {FaSignInAlt} from 'react-icons/fa'
+import {FaUser} from 'react-icons/fa'
+import {useSelector, useDispatch} from 'react-redux'
+import {useNavigate} from 'react-router-dom'
+import {login, reset} from '../features/auth/authSlice'
+import Spinner from '../components/Spinner'
+import {toast} from 'react-toastify'
 
 function Login() {
   const [formData, setFormData] = useState({
     email: '',
     password:'',
   })
+
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
+    const {user, isLoading, isError, isSuccess, message} = useSelector((state) => state.auth) 
+
+    useEffect(() => {
+        if(isError) {
+            toast.error(message)
+        }
+
+        if (isSuccess || user) {
+            navigate('/')
+        }
+
+        dispatch(reset())
+    }, [user, isError, isSuccess, isLoading, message, navigate, dispatch])
 
     const{email, password} = formData 
 
@@ -18,6 +41,16 @@ function Login() {
 
     const onSubmit = (e) => {
         e.preventDefault()
+
+        const userData = {
+            email, password
+        }
+
+        dispatch(login(userData))
+    }
+
+    if(isLoading) {
+        return <Spinner />
     }
 
     return (
