32a33ebae3a8af2f09086a452f8a403804aecc36
rstevens198 <ryanstevens198@yahoo.com>
2022-09-09 21:41:24 -0700

    deploy for render

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,15 @@ const { errorHandler } = require("../backend/middleware/errorMiddleware");
 const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
 
+if (process.env.NODE_ENV === "production") {
+  //*Set static folder up in production
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
+
 connectDB();
 
 const app = express();
